b942d447d5a98978adbe2be4d2436c44be9ca096
Oğuzhan Şenokur <44192576+oguzhansenokur@users.noreply.github.com>
2023-01-09 10:47:49 +0300

    testimonial component created

--- src/components/testimonial/index.jsx
+++ src/components/testimonial/index.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Testimonial() {
   return (
-    <div>Testimonial</div>
+    <div className='testimonial' >
+      <div className='testimonial__top' >
+        <div className='testimonial__top-header' ><h4>What our happy client say</h4></div>
+        <div className='testimonial__top-subHeader' ><h4>Several selected clients, who already believe in our service.</h4></div>
+      </div>
+      
+      <div className='testimonial__bottom' ></div>
+    </div>
   )
 }
